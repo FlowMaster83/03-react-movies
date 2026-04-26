89ca47316abc6762927f178368bc5b90f3708049
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2026-04-26 23:59:07 +0300

    added error message & loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,14 +4,17 @@ import MovieGrid from '../MovieGrid/MovieGrid'
 import { useState } from 'react'
 import type { Movie } from '../../types/movie'
 // import { Toaster } from 'react-hot-toast'
-
 import { fetchMovies } from '../../services/movieService'
 import MovieModal from '../MovieModal/MovieModal'
+import Loader from '../Loader/Loader'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
 
 function App() {
 
   const [movies, setMovies] = useState<Movie[]>([])
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   const openModal = (movie: Movie) => {
     setSelectedMovie(movie)
@@ -22,18 +25,35 @@ function App() {
   }
 
   const handleSearch = async (query: string) => {
-    const data = await fetchMovies(query)
-    setMovies(data)
+    setIsLoading(true)
+    setIsError(false)
+    setMovies([])
+
+    try {
+      const data = await fetchMovies(query)
+      setMovies(data)
+    } catch (error) {
+      setIsError(true)
+      console.error(error);
+    }
+    setIsLoading(false)
   }
 
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
-      <MovieGrid movies={movies} onSelect={openModal} />
+
+      {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
+
+      {!isLoading && !isError && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={openModal} />
+      )}
 
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={closeModal} />
       )}
+
     </div>
   )
 }
